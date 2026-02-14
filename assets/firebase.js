56e188aa2a05a74291ff49d22f09373f95836cc8
TheCoalNoob <adrianevincentjugo@gmail.com>
2026-02-14 09:14:21 +0800

    Create firebase.js

---
+++ assets/firebase.js
@@ -0,0 +1,18 @@
+// Firebase (CDN modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+
+// TODO: paste your Firebase config from Firebase Console → Project settings → Your apps
+const firebaseConfig = {
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  storageBucket: "PASTE_HERE",
+  messagingSenderId: "PASTE_HERE",
+  appId: "PASTE_HERE"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
