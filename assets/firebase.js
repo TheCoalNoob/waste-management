ec122df2ffb2597896a42490c29a3f89ecc5269c
TheCoalNoob <adrianevincentjugo@gmail.com>
2026-02-14 10:01:01 +0800

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,19 +1,14 @@
-// Firebase (CDN modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 
-// TODO: paste your Firebase config from Firebase Console → Project settings → Your apps
 const firebaseConfig = {
-  apiKey: "AIzaSyDBlM0r0Q5j9fPJaCX0X5lz77umYQ4GFF0",
-  authDomain: "waste-management-6b634.firebaseapp.com",
-  projectId: "waste-management-6b634",
-  storageBucket: "waste-management-6b634.firebasestorage.app",
-  messagingSenderId: "752464336479",
-  appId: "1:752464336479:web:132ad8ec5254901ad92dea",
-  measurementId: "G-C3CTZL55HK"
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  storageBucket: "PASTE_HERE",
+  messagingSenderId: "PASTE_HERE",
+  appId: "PASTE_HERE"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
